a083c8e05a923bf13169a5d4482955ac2d052656
djfacundoC <djfacundocoronel@gmail.com>
2026-03-30 10:05:48 -0300

    Estilos tailwinds al componente

--- src/components/CardBusiness/CardBusiness.jsx
+++ src/components/CardBusiness/CardBusiness.jsx
@@ -2,38 +2,42 @@ import style from './CardBusiness.module.css';
 
 const CardBusiness = () => {
    return (
-      <div>
+      <div className="w-full max-w-sm bg-white rounded-3xl shadow-md overflow-hidden hover:shadow-lg transition">
 
-         <div>
+         <div className="relative">
             <img
                src="https://images.unsplash.com/photo-1509042239860-f550ce710b93"
                alt="Green Bean Roastery"
+               className="w-full h-56 object-cover"
                loading="lazy"
             />
 
-            <div>
+            <div  className="absolute bottom-3 left-3 bg-white px-3 py-1 rounded-xl shadow text-sm font-medium flex items-center gap-1">
                📍 Brooklyn, NY
             </div>
          </div>
 
-         <div>
+         <div className="p-5">
 
-            <span>FAMILY OWNED</span>
+            <span className="inline-block text-xs font-semibold bg-red-100 text-red-500 px-2 py-1 rounded-md mb-3">FAMILY OWNED</span>
 
-            <h3>Green Bean Roastery</h3>
+            <h3 className="text-xl font-semibold text-gray-800">Green Bean Roastery</h3>
 
-            <p>
+            <p className="text-gray-500 text-sm mt-2 leading-relaxed line-clamp-3">
                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus vero vitae ducimus error quaerat laborum, omnis dicta officiis non repellendus perferendis consequuntur aperiam neque quo commodi qui tempora fugiat quae.
             </p>
 
-            <div>
-               <div>
-                  <span />
-                  <span />
-                  <span />
-               </div>
+        <div className="border-gray-200 border-t my-4"></div>
 
-               <span>2k+ local fans</span>
+        <div className="flex items-center justify-between">
+          
+          <div className="flex -space-x-2">
+            <div className="w-8 h-8 rounded-full bg-gray-300 border-2 border-white"></div>
+            <div className="w-8 h-8 rounded-full bg-gray-300 border-2 border-white"></div>
+            <div className="w-8 h-8 rounded-full bg-gray-300 border-2 border-white"></div>
+          </div>
+
+               <span className="text-sm text-gray-500 font-medium">2k+ local fans</span>
             </div>
 
          </div>
